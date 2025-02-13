bd5780ce66f7475cc88aada12b0b7b14460e2c68
deva-ghb <deva.macha@gytworkz.com>
2025-02-13 18:57:35 +0530

    edit : bedrock wrapper

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from botocore.exceptions import ClientError
 import uvicorn
 import requests
 from pydantic import BaseModel
+from dotenv import load_dotenv
+load_dotenv()
 
 client = boto3.client("bedrock-runtime", region_name="us-east-1")
 
@@ -31,14 +33,38 @@ def models():
     return {
         "object": "list",
         "data": [
-            # {
-            #     "id": "mistral.mistral-large-2402-v1:0",
-            #     "object": "model",
-            #     "created": 1734112601,
-            #     "owned_by": "system"
-            # },
             {
-                "id": "qwen2.5-coder:14b",
+                "id": "mistral.mistral-large-2402-v1:0",
+                "object": "model",
+                "created": 1734112601,
+                "owned_by": "system"
+            },
+            {
+                "id": "mistral.mistral-small-2402-v1:0",
+                "object": "model",
+                "created": 1734112601,
+                "owned_by": "system"
+            },
+            {
+                "id": "meta.llama3-3-70b-instruct-v1:0",
+                "object": "model",
+                "created": 1734112601,
+                "owned_by": "system"
+            },
+            {
+                "id": "meta.llama3-2-90b-instruct-v1:0",
+                "object": "model",
+                "created": 1734112601,
+                "owned_by": "system"
+            },
+            {
+                "id": "mistral.mixtral-8x7b-instruct-v0:1",
+                "object": "model",
+                "created": 1734112601,
+                "owned_by": "system"
+            },
+            {
+                "id": "anthropic.claude-3-5-sonnet-20240620-v1:0",
                 "object": "model",
                 "created": 1734112601,
                 "owned_by": "system"
@@ -46,7 +72,60 @@ def models():
         ]
     }
 
-@app.post("/v1/chat/my-completions")
+@app.post("/v1/chat/completions")
+async def v1_generate(inp: CompleationModel):
+    model = inp.model
+    messages = inp.messages
+    temperature = inp.temperature
+
+    # convert this into format for bedrock
+    bedrock_messages = []
+    for message in messages:
+        bedrock_messages.append({
+            "role": message.get("role"),
+            "content": [{"text": message.get("content")}]
+        })
+
+    try:
+        response = client.converse(
+            modelId=model,
+            messages=bedrock_messages,
+            inferenceConfig={"maxTokens": inp.max_tokens, "temperature": temperature, "topP": 0.9},
+        ) 
+        response_text = response["output"]["message"]["content"][0]["text"]
+        response = {
+            "id": response["ResponseMetadata"]["RequestId"],
+            "object": "chat.completion",
+            "created": int(datetime.utcnow().timestamp()), # current timestamp
+            "model": model,
+            "system_fingerprint": "fp_44709d6fcb",
+            "choices": [{
+                "index": 0,
+                "message": {
+                "role": "assistant",
+                "content": response_text,
+                },
+                "logprobs": None,
+                "finish_reason": "stop"
+            }],
+            "service_tier": "default",
+            "usage": {
+                "prompt_tokens": 9,
+                "completion_tokens": 12,
+                "total_tokens": 21,
+                "completion_tokens_details": {
+                "reasoning_tokens": 0,
+                "accepted_prediction_tokens": 0,
+                "rejected_prediction_tokens": 0
+                }
+            }
+        }
+        return response
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model}'. Reason: {e}")
+        exit(1)
+
+@app.post("/chat/completions")
 async def generate(inp: CompleationModel):
     model = inp.model
     messages = inp.messages
@@ -101,7 +180,7 @@ async def generate(inp: CompleationModel):
 
 
 TARGET_URL = "http://localhost:11434/v1/chat/completions"  # Change to the actual target service URL
-@app.post("/v1/chat/completions")
+@app.post("/v1/chat/my-completions")
 async def proxy_endpoint(request: Request):
     payload = await request.json()  # Read incoming payload
     
@@ -110,7 +189,7 @@ async def proxy_endpoint(request: Request):
     return response.json()  # Return the response from the target service
 
  # Change to the actual target service URL
-@app.post("/chat/completions")
+@app.post("/chat/my-completions")
 async def proxy_endpoint_v2(request: Request):
     payload = await request.json()  # Read incoming payload
     payload["stream"] = False
